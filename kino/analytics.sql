fb35af6c3dc0f5b50ea42a7d7f83e6b7b1b16f84
Kuba Janik <153774308+qbx11@users.noreply.github.com>
2026-01-12 13:15:05 +0100

    delete queries

--- kino/analytics.sql
+++
@@ -1,8 +0,0 @@
--- Ile łącznie sprzedano biletów?
-SELECT 
-    COUNT(*) as "Liczba sprzedanych biletów",
-    SUM(price) as "Łączny przychód (zł)",
-    ROUND(AVG(price), 2) as "Średnia cena biletu (zł)"
-FROM tickets t
-JOIN bookings b ON t.id_booking = b.id_booking
-WHERE b.booking_status = 'confirmed';
--- kino/queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT * 
-FROM tickets t
-JOIN bookings b ON t.id_booking = b.id_booking
-WHERE b.id_screening = 1 
-  AND t.row_num = 5 
-  AND t.seat_num = 15;
